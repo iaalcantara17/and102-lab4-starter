7ae1d8af713d93c5b982cb9f6dee0f28bf325427
Israel Alcantara <iaa48@njit.edu>
2026-02-27 23:20:29 -0500

    Added airplane mode compatibility to the app to let users still see information despite no Internet.

--- app/src/main/java/com/codepath/campgrounds/MainActivity.kt
+++ app/src/main/java/com/codepath/campgrounds/MainActivity.kt
@@ -3,12 +3,15 @@ package com.codepath.campgrounds
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.codepath.campgrounds.databinding.ActivityMainBinding
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.codepath.campgrounds.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
 import okhttp3.Headers
 import org.json.JSONException
@@ -44,6 +47,23 @@ class MainActivity : AppCompatActivity() {
         val campgroundAdapter = CampgroundAdapter(this, campgrounds)
         campgroundsRecyclerView.adapter = campgroundAdapter
 
+        lifecycleScope.launch {
+            (application as CampgroundApplication).db.campgroundDao().getAll().collect { databaseList ->
+                databaseList.map { entity ->
+                    Campground(
+                        entity.name,
+                        entity.description,
+                        entity.latLong,
+                        listOf(CampgroundImage(entity.imageUrl, null))
+                    )
+                }.also { mappedList ->
+                    campgrounds.clear()
+                    campgrounds.addAll(mappedList)
+                    campgroundAdapter.notifyDataSetChanged()
+                }
+            }
+        }
+
         campgroundsRecyclerView.layoutManager = LinearLayoutManager(this).also {
             val dividerItemDecoration = DividerItemDecoration(this, it.orientation)
             campgroundsRecyclerView.addItemDecoration(dividerItemDecoration)
@@ -70,12 +90,23 @@ class MainActivity : AppCompatActivity() {
                     )
 
                     // TODO: Do something with the returned json (contains campground information)
-                    val list = parsedJson.data ?: emptyList()
+                    parsedJson.data?.let { list ->
+                        lifecycleScope.launch(IO) {
+                            (application as CampgroundApplication).db.campgroundDao().deleteAll()
+                            (application as CampgroundApplication).db.campgroundDao().insertAll(
+                                list.map {
+                                    CampgroundEntity(
+                                        name = it.name,
+                                        description = it.description,
+                                        latLong = it.latLong,
+                                        imageUrl = it.imageUrl
+                                    )
+                                }
+                            )
+                        }
+                    }
 
                     // TODO: Save the campgrounds and reload the screen
-                    campgrounds.clear()
-                    campgrounds.addAll(list)
-                    campgroundAdapter.notifyDataSetChanged()
 
                 } catch (e: JSONException) {
                     Log.e(TAG, "Exception: $e")
